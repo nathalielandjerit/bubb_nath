df5c1f6be63a84fe94459d1eb7ab25b28e5aa9d9
Nathalie <nathalielandjerit@nathalie.local>
2015-11-05 23:12:20 +0100

    add project

---
+++ sass/config.rb
@@ -0,0 +1,8 @@
+http_path = "/"
+css_dir = "assets/css"
+sass_dir = "assets/sass"
+images_dir = "assets/img"
+javascript_dir = "assets/js"
+fonts_dir = "font"
+relavtive_assets = true
+line_comments = false
